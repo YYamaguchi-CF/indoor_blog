bda0525a3867ca2107d618048ae687fc49268f9c
Yuji.Yamaguchi <ieirireon@gmail.com>
2019-10-22 11:29:49 +0000

    ransack custom

--- db/migrate/20191022064325_add_columns_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :uid, :string
-    add_column :users, :oauth_token, :string
-    add_column :users, :oauth_expires_at, :string
-  end
-end
